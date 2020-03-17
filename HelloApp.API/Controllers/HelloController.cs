de52f5aa8714072c356e378c1439a23d22a45b85
Ervis Trupja <ervistrupja@outlook.com>
2020-03-17 19:31:56 +0100

    Add project files.

---
+++ HelloApp.API/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HelloApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from Web API");
+        }
+    }
+}
